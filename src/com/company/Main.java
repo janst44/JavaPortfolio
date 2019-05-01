61f91dfde7f72431f12db04a414eb9650f2f01f6
joshua <joshua.c4mpbell@gmail.com>
2019-05-01 14:57:29 -0600

    Adding Java Portfolio Project for accomplishing my java badge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        //Basic program
         System.out.println("What is your Name?");
         System.out.print(args[0] + " ");
         System.out.println(args[1]);
